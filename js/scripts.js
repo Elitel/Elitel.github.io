0ff7fe3de03a92a2338ca6a2dcc5c567369c8469
Elitel <Alitali@yandex.ru>
2018-12-11 22:51:57 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,6 @@ $(document).ready(function () {
         $(this).carousel('prev');
     })
 });
-AOS.init({
-    duration: 1000,
-});
 $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
         $('#hide').hide(1000);
@@ -17,14 +14,3 @@ $(window).scroll(function () {
         $('#hide').show(1000);
     }
 });
-
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(51526484, "init", {
-        id:51526484,
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });
