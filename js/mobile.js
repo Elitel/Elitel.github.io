62dc5052053907909813b465b9986c583e3f8e8b
Elitel <Alitali@yandex.ru>
2018-12-13 23:45:10 +0300

    adds

---
+++ js/mobile.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('#carousel').hammer().on('swipeleft', function () {
+        $(this).carousel('next');
+    })
+    $('#carousel').hammer().on('swiperight', function () {
+        $(this).carousel('prev');
+    })
+});
+
+$('.carousel').carousel({
+    interval: 2500,
+    pause: "hover",
+    keyboard: "true",
+})
+
